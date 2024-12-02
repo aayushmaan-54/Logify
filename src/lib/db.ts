2bb4644793f7bfb70619bc6e4cc4031a87a38843
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2024-12-02 21:07:25 +0530

    prod db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,20 @@
 import { Pool } from 'pg';
 
 
-export const pool = new Pool({
-  user: process.env.POSTGRESQL_USER,
-  host: process.env.POSTGRESQL_HOST,
-  database: process.env.POSTGRESQL_DB,
-  password: process.env.POSTGRESQL_PASSWORD,
-  port: Number(process.env.POSTGRESQL_PORT) || 5432, 
-});
+export const pool = process.env.NODE_ENV === 'development'
+  ? new Pool({
+    user: process.env.POSTGRESQL_USER,
+    host: process.env.POSTGRESQL_HOST,
+    database: process.env.POSTGRESQL_DB,
+    password: process.env.POSTGRESQL_PASSWORD,
+    port: Number(process.env.POSTGRESQL_PORT) || 5432,
+  })
+  : new Pool({
+    connectionString: process.env.PROD_VERCEL_NEON_DB_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  });
 
 
 export default async function dbConnect() {
